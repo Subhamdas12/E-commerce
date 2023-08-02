631e7735ab22760cbc471e970fa1247ef573dfdf
subham <sdas69629@gmail.com>
2023-08-02 20:46:10 +0530

    test

--- index.js
+++ index.js
@@ -177,6 +177,7 @@ const stripe = require("stripe")(process.env.STRIPE_SERVER_KEY);
 
 server.post("/create-payment-intent", async (req, res) => {
   const { totalAmount, orderId } = req.body;
+  console.log(totalAmount, orderId);
 
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
